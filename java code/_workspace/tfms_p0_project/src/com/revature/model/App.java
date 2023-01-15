9d3ac1b9738bf2f125e9fd5709ab9413ca6a69bf
aakashsolanke99 <aakashsolanke99@gmail.com>
2023-01-15 22:43:52 +0530

    javacode

---
+++ java code/_workspace/tfms_p0_project/src/com/revature/model/App.java
@@ -0,0 +1,9 @@
+package com.revature.model;
+public class App {
+
+	public static void main(String[] args) {
+      Menu menu= new Menu();
+      menu.showMenu();
+
+}
+} 
---
+++ java code/_workspace/tfms_p0_project/src/com/revature/util/TimeConversion.java
@@ -0,0 +1,10 @@
+package com.revature.util;
+
+public class TimeConversion {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
